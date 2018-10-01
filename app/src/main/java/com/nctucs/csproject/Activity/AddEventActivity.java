791995c7e0a6a501dcfde17ce2fb4a9f9213d82d
wu <local111043@gmail.com>
2018-10-01 19:03:28 +0800

    add new package

---
+++ app/src/main/java/com/nctucs/csproject/Activity/AddEventActivity.java
@@ -0,0 +1,4 @@
+package com.nctucs.csproject.Activity;
+
+public class AddEventActivity {
+}
--- app/src/main/java/com/nctucs/csproject/Activity/WelComeActivity.java
+++ app/src/main/java/com/nctucs/csproject/Activity/WelComeActivity.java
@@ -1,19 +1,15 @@
-package Activity;
+package com.nctucs.csproject.Activity;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -21,14 +17,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.nctucs.csproject.R;
 
 import static android.content.ContentValues.TAG;
